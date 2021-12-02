5464a78031c023a4625b42f48adc71bcfad357ed
tarkanoz <40675384+tarkanoz@users.noreply.github.com>
2021-12-02 17:03:24 +0300

    Add files via upload

---
+++ Yandex Translate Apı/app.js
@@ -0,0 +1,32 @@
+eventListeners();
+
+function eventListeners(){
+    document.getElementById("translate-form").addEventListener("submit",translateWord)
+     //change
+     document.getElementById("language").onchange = function(){
+         //arayüz işlemleri
+    
+          ui.changeUI();
+    
+        }
+
+}
+
+const translate = new Translate(document.getElementById("word").value,document.getElementById("language").value)
+const ui = new UI();
+
+function translateWord(e){
+
+    translate.changeParameters(document.getElementById("word").value,document.getElementById("language").value)
+    translate.translateWord(function(err,response){
+         if(err){
+             //hata
+             console.log(err);
+         }
+         else{
+             ui.displayTranslate(response);
+         }
+    });
+
+    e.preventDefault();
+}
---
+++ Yandex Translate Apı/ui.js
@@ -0,0 +1,21 @@
+function UI(){
+
+    this.outputImage = document.getElementById("outputImage");
+    this.outputLanguage = document.getElementById("outputLanguage");
+    this.outputWord = document.getElementById("outputWord");
+
+    this.languageList = document.getElementById("language");
+   
+
+}
+
+UI.prototype.changeUI = function(){
+    //arayüz değiştir
+    this.outputImage.src = `images/${this.languageList.value}.png`
+    this.outputLanguage.innerHTML = this.languageList.options[this.languageList.selectedIndex].textContent;
+
+}
+
+UI.prototype.displayTranslate = function(word){
+    this.outputWord.textContent = word;
+}
